4ab95f0437e121be1a083a61c10d492186b4c4e2
Alator <botazz133@gmail.com>
2019-03-14 19:37:49 +0200

    Prettier display of number of kills

--- Trench.js
+++ Trench.js
@@ -1,21 +1,21 @@
 'use strict';
 class Trench extends Boid {
-    constructor(x, y, size, velocity, image, imageFlipped) {
-        super(x, y, size, velocity, image, imageFlipped);
-        this.maxSpeed = 3.5;
-        this.maxForce = 0.1;
-        this.color = createVector(90, 0, 0);
-    }
+	constructor(x, y, size, velocity, image, imageFlipped) {
+		super(x, y, size, velocity, image, imageFlipped);
+		this.maxSpeed = 3.5;
+		this.maxForce = 0.1;
+		this.color = createVector(90, 0, 0);
+	}
 
-    show() {
-        if (!globalShowNames && globalShowKills) {
-            if (this.numberOfKills > 0) {
-                stroke(150, 0, 0);
-                noFill();
-                textSize(15);
-                text(this.numberOfKills, this.position.x, this.position.y - this.size / 2);
-            }
-        }
-        super.show();
-    }
+	show() {
+		if (!globalShowNames && globalShowKills) {
+			if (this.numberOfKills > 0) {
+				noStroke();
+				fill(150, 0, 0);
+				textSize(15);
+				text(this.numberOfKills, this.position.x, this.position.y - this.size / 2);
+			}
+		}
+		super.show();
+	}
 }
